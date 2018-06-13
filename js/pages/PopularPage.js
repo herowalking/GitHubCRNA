ae51f3704d52d97c1fd9349e3a91d1b5433b7cfb
Lincoln <lincolnpb@hotmail.com>
2018-06-13 13:20:30 +0800

    使用FlatList展示列表数据。

--- js/pages/PopularPage.js
+++ js/pages/PopularPage.js
@@ -2,16 +2,25 @@
  * Created by Lincoln on 6/13/2018.
  */
 import React from 'react';
-import {StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, FlatList } from 'react-native';
 import NavigationBar from '../components/NavigationBar';
 
 //包含两块内容，状态栏（静），滚动视图（动）
 export default class PopularPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSource: ['Hello', 'World', 'Cup']
+        }
+    }
     render() {
         return (
             <View style={styles.container}>
                 <NavigationBar/>
-                <View style={{backgroundColor:'tomato', flex:1}}></View>
+                <FlatList
+                    data={this.state.dataSource}
+                    renderItem={({item}) => <Text>{item}</Text>}
+                />
             </View>
         );
     }
